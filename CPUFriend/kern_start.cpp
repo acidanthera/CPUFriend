be3261bd4839ce1b883759b28bfe425861105588
PMheart <angelwvanilla@yahoo.com>
2018-06-06 12:42:54 +0800

    Native 10.14 support.
    Lilu 1.2.4+ required!

--- CPUFriend/kern_start.cpp
+++ CPUFriend/kern_start.cpp
@@ -35,7 +35,7 @@ PluginConfiguration ADDPR(config) {
 	bootargBeta,
 	arrsize(bootargBeta),
 	KernelVersion::MountainLion,
-	KernelVersion::HighSierra,
+	KernelVersion::Mojave,
 	[]() {
 		cpuf.init();
 	}
